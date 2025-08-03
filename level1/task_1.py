08e780d3656471f8c0820f4ec1017eb8826f7bc7
ROHIT-NIMASE <rohitnimase11100@gmail.com>
2025-08-03 18:21:38 +0530

    Update task_1.py

--- level1/task_1.py
+++ level1/task_1.py
@@ -1,12 +1,11 @@
 def reverse_string(s):
     return s[::-1]
 
-# Take input from the user
 user_input = input("Enter a string: ")
 reversed_str = reverse_string(user_input)
 print("Reversed string:", reversed_str)
  
 #input 
 #enter a string : hello
-# output
-# Reversed string : olleh  
+#output
+#Reversed string : olleh  
