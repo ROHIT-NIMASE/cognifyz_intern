38843ba947adbee1cba27d1b36962ffdc7acc7ba
ROHIT-NIMASE <rohitnimase11100@gmail.com>
2025-08-03 18:18:53 +0530

    Add files via upload

---
+++ level1/task_1.py
@@ -0,0 +1,12 @@
+def reverse_string(s):
+    return s[::-1]
+
+# Take input from the user
+user_input = input("Enter a string: ")
+reversed_str = reverse_string(user_input)
+print("Reversed string:", reversed_str)
+ 
+#input 
+#enter a string : hello
+# output
+# Reversed string : olleh  
